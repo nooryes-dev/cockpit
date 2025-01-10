204d402fad0a9cc4d9e08f94619627fa89c99931
murukal <tutu@fantufantu.com>
2025-01-10 23:26:54 +0800

    feat: integrate TypeORM for TechStack module with database support

--- src/tech-stack/tech-stack.module.ts
+++ src/tech-stack/tech-stack.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TechStackService } from './tech-stack.service';
 import { TechStackController } from './tech-stack.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TechStack } from '@/libs/database';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TechStack])],
   controllers: [TechStackController],
   providers: [TechStackService],
 })
