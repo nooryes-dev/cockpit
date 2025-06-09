d0e111ac3275e975a117d28f42f8cb65c6efea20
qianxu.ljj <qianxu.ljj@alibaba-inc.com>
2025-06-09 20:48:04 +0800

    feat: 优化生产问题的prompt

--- src/exam/prompts/question.prompt.ts
+++ src/exam/prompts/question.prompt.ts
@@ -5,7 +5,7 @@ const template = ChatPromptTemplate.fromMessages([
   ChatPromptTemplate.fromTemplate(
     '你现在是一个{position}的面试官，本次面试需要考察候选人的能力，请你给出10个合理的面试问题。',
   ),
-  `每个面试问题希望通过${SPEARATOR}来分割`,
+  `每个面试问题希望通过${SPEARATOR}来分割，不要添加任何其他内容`,
 ]);
 
 export const useQuestionsPrompt = (position: string) => {
