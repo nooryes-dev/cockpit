e9fb888d63702fa75a6de82b1b11bdd1dee14974
murukal <tutu@fantufantu.com>
2025-06-15 21:39:52 +0800

    chore: 移除分页请求格式中的验证装饰器

--- typings/pagination.types.ts
+++ typings/pagination.types.ts
@@ -1,5 +1,4 @@
 import { ApiProperty, ApiSchema } from '@nestjs/swagger';
-import { IsNumber, Min } from 'class-validator';
 
 @ApiSchema({ description: '分页请求格式' })
 export class Pagination {
@@ -8,8 +7,6 @@ export class Pagination {
     type: Number,
     default: 1,
   })
-  @IsNumber(void 0, { message: '请输入页码' })
-  @Min(1)
   page: number;
 
   @ApiProperty({
@@ -17,8 +14,6 @@ export class Pagination {
     type: Number,
     default: 10,
   })
-  @IsNumber(void 0, { message: '请输入每页数量' })
-  @Min(1)
   pageSize: number;
 }
 
