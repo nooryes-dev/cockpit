7ed3aaf676b60c69d047bb57ed25a2cee6fd2ffd
murukal <tutu@fantufantu.com>
2025-07-09 22:42:48 +0800

    feat: 更新评分模板，调整评分和评价的顺序，优化示例格式

--- src/exam/prompts/review.prompt.ts
+++ src/exam/prompts/review.prompt.ts
@@ -2,17 +2,26 @@ import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { SCORE_SEPARATOR, SPEARATOR } from '../constants';
 // import { at } from '@aiszlab/relax';
 
+const TOTAL = 10;
+
+const TEMPLATE_SCORES = Array.from({ length: TOTAL }).map(() => {
+  return Math.ceil(Math.random() * 100);
+});
+const TOTAL_SCORE = TEMPLATE_SCORES.reduce((prev, cur) => prev + cur, 0) / 10;
+
+console.log('TOTAL_SCORE===', TOTAL_SCORE);
+
 const template = ChatPromptTemplate.fromMessages([
   ChatPromptTemplate.fromTemplate(
     `你现在是一个{position}的面试官，本次面试过程中，候选人针对以下10个问题进行了回答，问题依次是：
-${Array.from({ length: 10 })
+${Array.from({ length: TOTAL })
   .map((_, index) => {
     const _index = index + 1;
     return `${_index}. {question${_index}}`;
   })
   .join('；')}。
 候选人的回答依次是：
-${Array.from({ length: 10 })
+${Array.from({ length: TOTAL })
   .map((_, index) => {
     const _index = index + 1;
     return `${_index}. {answer${_index}}`;
@@ -20,10 +29,15 @@ ${Array.from({ length: 10 })
   .join('；')}。`,
   ),
   `
-请你针对问题和回答给候选人打分，先给一个总分，然后每一题请给出详细的评价和评分（用百分制）
-评价和分数之间使用${SCORE_SEPARATOR}分隔，每题之间使用${SPEARATOR}，内容不要添加换行符
-示例如下：
-80${SPEARATOR}评价内容${SCORE_SEPARATOR}80${SPEARATOR}评价内容${SCORE_SEPARATOR}90${SPEARATOR}评价内容${SCORE_SEPARATOR}70
+请你针对全部问题和回答给候选人打分，先给一个总分，然后每一题请给出评分（用百分制）和详细的评价。
+总分和每题结果之间使用${SPEARATOR}分隔，评分和评价之间使用${SCORE_SEPARATOR}分隔，每题结果之间使用${SPEARATOR}分隔。
+注意：分隔符前后不要添加换行符。
+以下是3题的示例，其他数量题目以此类推：
+${TOTAL_SCORE}${SPEARATOR}${Array.from({ length: TOTAL })
+    .map((_, index) => {
+      return `${TEMPLATE_SCORES[index]}${SCORE_SEPARATOR}详细的评价`;
+    })
+    .join(SPEARATOR)}
 `,
 ]);
 
